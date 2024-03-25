ce6ef412af348612f3a3f24365609d5fc8e34683
manyamkrishnakumar <prodotmanyam@gmail.com>
2024-03-25 14:57:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import pri from './pri.jpg'
 
-function App() {
+// const App = () => {
+//   return (
+//     <div>
+//       <center>
+//         <img src={pri} alt='pri' height="250" width= "auto"/><br/><br/>
+//         <a href={pri} className='btn btn-primary' download = "priya">click Hear to Dowload</a>
+//       </center>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// Background Color changer
+
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [color,setColor] = useState("olive")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container' style={{backgroundColor:color}}>
+    <div className='buttonSection'>
+    <button onClick={()=>setColor("red")} style={{backgroundColor:"red", color:"white", padding:"5px", margin:"10px"}}>Red</button>
+    <button onClick={()=>setColor("green")} style={{backgroundColor:"green", color:"white", padding:"5px", margin:"10px"}}>Green</button>
+    <button onClick={()=>setColor("blue")} style={{backgroundColor:"blue", color:"white", padding:"5px", margin:"10px"}}>Blue</button>
+
     </div>
-  );
+
+    </div>
+  )
 }
 
-export default App;
+export default App
